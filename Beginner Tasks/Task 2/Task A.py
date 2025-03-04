da184eb6493045fa33ea79c7c49e9d1aa2630ab7
JunkyProgrammer <73176056+junkyprogrammer@users.noreply.github.com>
2025-03-04 10:46:40 +0530

    Completed Task A

--- Beginner Tasks/Task 2/Task A.py
+++ Beginner Tasks/Task 2/Task A.py
@@ -1,1 +1,14 @@
+#Write a function that takes two arguments, 145 and 'o' , and uses the `format` function to return a formatted string. Print the result. Try to identify the representation used.
 
+# Two arguements are passed 
+# function definition
+
+def displayMessage(arguement1, arguement2):
+  print(arguement1 + " " + arguement2)
+  
+  
+# Function call
+displayMessage("145", "o")
+
+
+#Displaying a simple Non errorous output showing : 154 o
