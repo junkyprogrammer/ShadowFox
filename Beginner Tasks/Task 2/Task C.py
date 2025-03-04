ca86583a65862e55cc599bc7d911c37000bce81c
JunkyProgrammer <73176056+junkyprogrammer@users.noreply.github.com>
2025-03-04 11:15:53 +0530

    Completed Task C

---
+++ Beginner Tasks/Task 2/Task C.py
@@ -0,0 +1,22 @@
+#If you cross a 490meterlong street in 7 minutes, calculate your speed in meters per second. Print the answer without any decimal point in it. Hint: Speed = Distance / Time
+
+streetLength = 490
+time = 7
+
+# Definfing it's formula
+
+speed = (streetLength)/7
+
+#Converting Minutes into Seconds
+
+result=(speed)/60
+
+#Converting the float value into Integer Value
+
+fspeed=int(result)
+
+#printing the result
+
+print("Speed : ",fspeed,"m/s" " " "approx")
+
+
