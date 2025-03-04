3e2a29477132098884ca387781bc3c3018317f0b
JunkyProgrammer <73176056+junkyprogrammer@users.noreply.github.com>
2025-03-04 10:31:56 +0530

    Completed Task B

---
+++ Beginner Tasks/Task 01 Variables/TASK B.py
@@ -0,0 +1,7 @@
+#Task B
+#Description : Create a variable called for and assign it a value 4. See what happens and find out the reason behind the  behavior that you see.
+
+for = 4
+print(for)
+
+# Created a varible called 'for'and assigned it a value 4. It's giving an output as Syntax Error: invalid syntax
